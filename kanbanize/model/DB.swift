d9f8d523aa704d61f2c7b9cfd7ed49e36cb64309
Peter Ho <peterho@Peters-Mac.local>
2018-04-18 10:00:32 +0700

    add contrains and DB class

---
+++ kanbanize/model/DB.swift
@@ -0,0 +1,29 @@
+//
+//  DB.swift
+//  kanbanize
+//
+//  Created by Peter Ho on 4/18/18.
+//  Copyright © 2018 Peter Ho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class DB {
+    static var MOC: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    static func save() {
+        if MOC.hasChanges {
+            do {
+                try MOC.save()
+            }
+            catch let error as NSError {
+                print("Cannot save db \(error)")
+            }
+        }
+    }
+}

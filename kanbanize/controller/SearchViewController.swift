23e5a923666476509d785b17c8c128bd9b6ca8c9
Peter Ho <peterho@Peters-Mac.local>
2018-05-16 10:54:52 +0700

    Add search bar

--- kanbanize/controller/SearchViewController.swift
+++ kanbanize/controller/SearchViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // MARK - Variable
     var boardName = ""
+    @IBOutlet weak var tableView: UITableView!
+    var searchController: UISearchController!
+    var tasks: [NSManagedObject] = []
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as! TaskTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +33,38 @@ class SearchViewController: UIViewController {
         self.tabBarController?.navigationItem.rightBarButtonItems = []
         self.title = "Search"
         self.tabBarController?.navigationItem.title = "Search"
+        
+        tasks = Task.All()
+        
+        let resultsController = SearchResultController()
+        resultsController.tasks = tasks
+        searchController = UISearchController(searchResultsController: resultsController)
+        
+        let searchBar = searchController.searchBar
+        searchBar.scopeButtonTitles = ["Name", "Label"]
+        searchBar.placeholder = "Enter a search term"
+        searchBar.sizeToFit()
+        tableView.tableHeaderView = searchBar
+        searchController.searchResultsUpdater = resultsController
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.navigationItem.rightBarButtonItems = []
         self.title = "Search"
         self.tabBarController?.navigationItem.title = "Search"
+        
+        tasks = Task.All()
+        
+        let resultsController = SearchResultController()
+        resultsController.tasks = tasks
+        searchController = UISearchController(searchResultsController: resultsController)
+        
+        let searchBar = searchController.searchBar
+        searchBar.scopeButtonTitles = ["Name", "Label"]
+        searchBar.placeholder = "Enter a search term"
+        searchBar.sizeToFit()
+        tableView.tableHeaderView = searchBar
+        searchController.searchResultsUpdater = resultsController
     }
 
     override func didReceiveMemoryWarning() {

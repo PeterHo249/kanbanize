ef9664c86212fd0816d4f78a7f50c75de268ffa9
Peter Ho <peterho@Peters-Mac.local>
2018-05-17 19:09:01 +0700

    Fix search view

--- kanbanize/controller/SearchViewController.swift
+++ kanbanize/controller/SearchViewController.swift
@@ -9,22 +9,16 @@
 import UIKit
 import CoreData
 
-class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SearchViewController: UIViewController {
 
     // MARK - Variable
     var boardName = ""
     @IBOutlet weak var tableView: UITableView!
     var searchController: UISearchController!
     var tasks: [NSManagedObject] = []
+    var filteredTasks: [NSManagedObject] = []
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as! TaskTableViewCell
-        return cell
-    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,18 +28,24 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         self.title = "Search"
         self.tabBarController?.navigationItem.title = "Search"
         
+        tableView.register(TaskTableViewCell.self, forCellReuseIdentifier: "TaskTableViewCell")
+        let xib = UINib(nibName: "TaskTableCell", bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: "TaskTableViewCell")
+        tableView.rowHeight = 70
+        
         tasks = Task.FetchData(sort: false, board: boardName)
         
-        let resultsController = SearchResultController()
-        resultsController.tasks = tasks
-        searchController = UISearchController(searchResultsController: resultsController)
+        searchController = UISearchController(searchResultsController: nil)
         
-        let searchBar = searchController.searchBar
-        searchBar.scopeButtonTitles = ["Name", "Label"]
-        searchBar.placeholder = "Enter a search term"
-        searchBar.sizeToFit()
-        tableView.tableHeaderView = searchBar
-        searchController.searchResultsUpdater = resultsController
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Tasks"
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.scopeButtonTitles = ["Name", "Label"]
+        tableView.tableHeaderView = searchController.searchBar
+        tableView.contentInset = UIEdgeInsets(top: 70, left: 0, bottom: 0, right: 0)
+        definesPresentationContext = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -54,17 +54,6 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         self.tabBarController?.navigationItem.title = "Search"
         
         tasks = Task.FetchData(sort: false, board: boardName)
-        
-        let resultsController = SearchResultController()
-        resultsController.tasks = tasks
-        searchController = UISearchController(searchResultsController: resultsController)
-        
-        let searchBar = searchController.searchBar
-        searchBar.scopeButtonTitles = ["Name", "Label"]
-        searchBar.placeholder = "Enter a search term"
-        searchBar.sizeToFit()
-        tableView.tableHeaderView = searchBar
-        searchController.searchResultsUpdater = resultsController
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,6 +61,17 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
+    func ViewDetailAction(index: Int) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let editVC = storyBoard.instantiateViewController(withIdentifier: "TaskDetailViewController") as! TaskDetailViewController
+        editVC.taskInfo = filteredTasks[index] as! Task
+        editVC.selectedIndex = index
+        editVC.modeFlag = false
+        editVC.sourceViewController = self
+        editVC.sourceStatus = (filteredTasks[index] as! Task).status
+        editVC.currentBoard = (filteredTasks[index] as! Task).board
+        self.tabBarController?.navigationController?.pushViewController(editVC, animated: true)
+    }
 
     /*
     // MARK: - Navigation
@@ -83,4 +83,49 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
     }
     */
 
+}
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as! TaskTableViewCell
+        let taskInfo = filteredTasks[indexPath.row] as! Task
+        
+        cell.LoadContent(name: taskInfo.name!, dueDate: taskInfo.dueDate! as Date, status: taskInfo.status!)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredTasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        ViewDetailAction(index: indexPath.row)
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchString = searchController.searchBar.text {
+            let buttonIndex = searchController.searchBar.selectedScopeButtonIndex
+            filteredTasks.removeAll(keepingCapacity: true)
+            
+            if buttonIndex == 0 {
+                for task in tasks {
+                    if (((task as! Task).name?.range(of: searchString, options: .caseInsensitive, range: nil, locale: nil)) != nil) {
+                        filteredTasks.append(task)
+                    }
+                }
+            } else {
+                for task in tasks {
+                    if (((task as! Task).label?.range(of: searchString, options: .caseInsensitive, range: nil, locale: nil)) != nil) {
+                        filteredTasks.append(task)
+                    }
+                }
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    
 }
